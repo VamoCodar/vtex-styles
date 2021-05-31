33eede99b857954b645a83558fc2a1596f666bf7
adr <senhoraroldo@gmail.com>
2021-05-31 15:42:33 -0300

    olaaa

--- menu.js
+++ menu.js
@@ -1,3 +1,18 @@
 const topMenu = document.querySelector(".header__top-menu")
 const headercontent = document.querySelector(".header__content")
 headercontent.appendChild(topMenu)
+
+
+function menuPreenchido() {
+    const menuFixo = document.querySelectorAll(".menu");
+    menuFixo.forEach((i) => {
+        if (window.pageYOffset >= 120) {
+            document.body.classList.add("ativo__scroll");
+
+        } else if (window.pageYOffset < 150) {
+            document.body.classList.remove("ativo__scroll");
+        }
+    });
+}
+menuPreenchido();
+window.addEventListener("scroll", menuPreenchido);
